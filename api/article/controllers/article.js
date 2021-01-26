fa82c1e626177837fcf9ec3bf062f9b55445e393
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2021-01-25 22:14:11 -0500

    check if a user has a premium subscription to gate premium content

--- api/article/controllers/article.js
+++ api/article/controllers/article.js
@@ -1,4 +1,4 @@
-const { sanitizeEntity } = require('strapi-utils');
+const { sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
   /**
@@ -7,10 +7,70 @@ module.exports = {
    * @return {Object}
    */
 
-  async findOne(ctx) {
+  findOne: async (ctx) => {
     const { slug } = ctx.params;
 
     const entity = await strapi.services.article.findOne({ slug });
+
+    // Check if this is premium content and that the user has an active subscription
+    if (entity.premium) {
+      if (ctx.user) {
+        const { premiumUntil } = ctx.state.user;
+        if (premiumCurrent(premiumUntil)) {
+          return sanitizeEntity(entity, { model: strapi.models.article });
+        }
+      } else {
+        entity.content =
+          "This is premium content, sign up to read the rest of the article.";
+        return sanitizeEntity(entity, { model: strapi.models.article });
+      }
+    }
+
     return sanitizeEntity(entity, { model: strapi.models.article });
   },
+
+  /**
+   * Retrieve all articles with the content stripped to reduce over the wire data
+   *
+   * @return {Object}
+   */
+
+  find: async (ctx) => {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.article.search(ctx.query);
+    } else {
+      entities = await strapi.services.article.find(ctx.query);
+    }
+
+    return entities.map((entity) => {
+      entity.content = "";
+      return sanitizeEntity(entity, { model: strapi.models.article });
+    });
+  },
+};
+
+/**
+ *
+ * @param {Date} premiumDate
+ *
+ * Checks if the users premium until date is greater then
+ * todays date. If the users premium subscription is not current
+ * then return false
+ *
+ * @returns {bool}
+ */
+const premiumCurrent = function (premiumDate) {
+  if (!premiumDate) {
+    return false;
+  }
+
+  let today = new Date();
+  let goodUntil = new Date(premiumDate);
+
+  if (today.setHours(0, 0, 0, 0) <= goodUntil.setHours(0, 0, 0, 0)) {
+    return true;
+  } else {
+    return false;
+  }
 };
