746596e7660d8a81b11690b19b27cf9fd036beec
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2021-01-29 21:25:16 -0500

    create custom aws s3 provider because npm package does not provide options for our use case

--- config/env/development/plugins.js
+++ config/env/development/plugins.js
@@ -3,5 +3,19 @@ module.exports = ({ env }) => {
     email: {
       provider: "console",
     },
+
+    upload: {
+      provider: "s3",
+      providerOptions: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_ACCESS_SECRET,
+        region: process.env.AWS_REGION,
+        publicFiles: false,
+        baseUrl: "https://d2mc8fs8vjub2b.cloudfront.net",
+        params: {
+          Bucket: process.env.AWS_BUCKET,
+        },
+      },
+    },
   };
 };
