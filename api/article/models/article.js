872c23e50663e04121540aba2cc2145f7aa4ceae
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2021-01-30 13:08:59 -0500

    setup redeploy webhook

--- api/article/models/article.js
+++ api/article/models/article.js
@@ -1,8 +1,31 @@
-'use strict';
+"use strict";
+const axios = require("axios").default;
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    afterUpdate: async (entry) => {
+      if (process.env.NODE_ENV === "production") {
+        axios
+          .post(
+            `${proccess.env.DIGITAL_OCEAN_DEPLOY_API}`,
+            {
+              force_build: true,
+            },
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${process.env.DIGITAL_OCEAN_API_KEY}`,
+              },
+            }
+          )
+          .then((response) => console.log("Rebuilding Front End"))
+          .catch((err) => console.log(err.message));
+      }
+    },
+  },
+};
