46065aead00826540bc0380321804c51c848adc7
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2021-01-29 19:16:31 -0500

    update s3 plugin to not use public acccess and update cloudfront url

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -6,6 +6,8 @@ module.exports = ({ env }) => {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_ACCESS_SECRET,
         region: process.env.AWS_REGION,
+        publicFiles: false,
+        baseUrl: 'https://d2mc8fs8vjub2b.cloudfront.net/killerwhale',
         params: {
           Bucket: process.env.AWS_BUCKET,
         },
@@ -26,6 +28,6 @@ module.exports = ({ env }) => {
         defaultFrom: process.env.EMAIL_ADDRESS_FROM,
         defaultReplyTo: process.env.EMAIL_ADDRESS_REPLY,
       },
-    };
+    }
   };
 };
