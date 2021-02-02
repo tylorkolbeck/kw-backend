2fe18d1d834c87a4222a3003f9f73e25cc245661
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2021-02-01 19:47:38 -0500

    add counter to prevent a million rebuilds on content change

--- api/article/models/article.js
+++ api/article/models/article.js
@@ -5,27 +5,49 @@ const axios = require("axios").default;
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
+let COOLDOWN = 10 * 60 * 1000;
+let intervalId;
+let countdown = COOLDOWN;
+
+function startRebuildCountdown() {
+  clearInterval(intervalId);
+  countdown = COOLDOWN;
+  intervalId = setInterval(() => {
+    countdown -= 5000;
+    if (countdown <= 0) {
+      callRebuildHook();
+      clearInterval(intervalId);
+    }
+  }, 5000);
+}
+
+function callRebuildHook() {
+  if (process.env.NODE_ENV === "production") {
+    axios
+      .post(
+        `${proccess.env.DIGITAL_OCEAN_DEPLOY_API}`,
+        {
+          force_build: true,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.DIGITAL_OCEAN_API_KEY}`,
+          },
+        }
+      )
+      .then((response) => console.log("Rebuilding Front End"))
+      .catch((err) => console.log(err.message));
+  }
+}
 
 module.exports = {
   lifecycles: {
+    afterCreate: async (entry) => {
+      startRebuildCountdown();
+    },
     afterUpdate: async (entry) => {
-      if (process.env.NODE_ENV === "production") {
-        axios
-          .post(
-            `${proccess.env.DIGITAL_OCEAN_DEPLOY_API}`,
-            {
-              force_build: true,
-            },
-            {
-              headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${process.env.DIGITAL_OCEAN_API_KEY}`,
-              },
-            }
-          )
-          .then((response) => console.log("Rebuilding Front End"))
-          .catch((err) => console.log(err.message));
-      }
+      startRebuildCountdown();
     },
   },
 };
