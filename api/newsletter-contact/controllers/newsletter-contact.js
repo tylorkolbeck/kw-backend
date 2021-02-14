524155ad00b748b492f496df80b9c0fdfefcf30a
jkaplanis <johntkaplanis@gmail.com>
2021-02-13 22:46:39 -0800

    create newsletter contact collection and findOne endpoint to return requested email

--- api/newsletter-contact/controllers/newsletter-contact.js
+++ api/newsletter-contact/controllers/newsletter-contact.js
@@ -1,8 +1,24 @@
-'use strict';
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  /**
+   * Retrieve a record.
+   *
+   * @return {Object}
+   */
+  async findOne(ctx) {
+    const { email } = ctx.params;
+    console.log(email);
+    const entity = await strapi.services["newsletter-contact"].findOne({
+      email: email,
+    });
+    return sanitizeEntity(entity, {
+      model: strapi.models["newsletter-contact"],
+    });
+  },
+};
