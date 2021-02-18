14e0fa7d46f7055f1177c34a9b44e7fd11a44423
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2021-02-18 09:22:44 -0500

    add environment variable for announcement channel and frontend url

--- api/article/models/article.js
+++ api/article/models/article.js
@@ -2,53 +2,10 @@
 const axios = require("axios").default;
 const { DateTime } = require("luxon");
 
-let http = require("http");
-
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
-let COOLDOWN = 10 * 60 * 1000;
-let intervalId;
-let countdown = COOLDOWN;
-
-function startRebuildCountdown() {
-  // console.log("START REBUILD");
-  // clearInterval(intervalId);
-  // countdown = COOLDOWN;
-  // intervalId = setInterval(() => {
-  //   countdown -= 5000;
-  //   if (countdown <= 0) {
-  //     callRebuildHook();
-  //     clearInterval(intervalId);
-  //   } else {
-  //     console.log("COUNTDOWN", countdown);
-  //   }
-  // }, 5000);
-  // callRebuildHook();
-}
-
-function callRebuildHook() {
-  console.log("CALLING WEBHOOK FUNCTION", process.env.NODE_ENV);
-  if (process.env.NODE_ENV !== "development") {
-    console.log("CALLING WEBHOOK - SHOULD SEE REBUILD");
-    axios
-      .post(
-        `${proccess.env.DIGITAL_OCEAN_DEPLOY_API}`,
-        {
-          force_build: true,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${process.env.DIGITAL_OCEAN_API_KEY}`,
-          },
-        }
-      )
-      .then((response) => console.log("Rebuilding Front End"))
-      .catch((err) => console.log(err.message));
-  }
-}
 
 module.exports = {
   lifecycles: {
@@ -66,13 +23,23 @@ module.exports = {
       if (secondsSincePublished < 10) {
         try {
           setTimeout(() => {
-            strapi.services.bot.sendBlogPostToDiscord("announcements", {
-              title: entry.title,
-              description: entry.description,
-              url: `https://kw-frontend-abs5o.ondigitalocean.app/article/${entry.slug}`,
-              author: `Checkout the new post on our website by ${entry.author.name}`,
-              thumbnail: entry.image.formats.thumbnail.url,
-            });
+            axios
+              .get(`${process.env.FRONT_END_URL}/article/${entry.slug}`)
+              .then((res) => {
+                strapi.services.bot.sendBlogPostToDiscord(
+                  process.env.DISCORD_ANNOUNCEMENT_CHANNEL_ID,
+                  {
+                    title: entry.title,
+                    description: entry.description,
+                    url: `${process.env.FRONT_END_URL}/article/${entry.slug}`,
+                    author: `Checkout the new post on our website by ${entry.author.name}`,
+                    thumbnail: entry.image.formats.thumbnail.url,
+                  }
+                );
+              })
+              .catch(() => {
+                console.log("Error getting new blog post");
+              });
           }, 60000);
         } catch (err) {
           console.log(err);
--- config/discord/client.js
+++ config/discord/client.js
@@ -1,4 +1,9 @@
 const { Client, Intents } = require("discord.js");
+// Keep these comments here for future feature where
+// the bot may want to control the users in discord
+// so that it can have access to the entire dicord
+// memberlist.
+
 // let intents = new Intents(Intents.NON_PRIVILEGED);
 // intents.add("GUILD_MEMBERS");
 
