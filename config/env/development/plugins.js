0822e97637521f369f7f0e0a2790143f9583b193
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2021-08-08 14:55:13 -0400

    add testimonial model

--- config/env/development/plugins.js
+++ config/env/development/plugins.js
@@ -17,5 +17,21 @@ module.exports = ({ env }) => {
         },
       },
     },
+
+    email: {
+      provider: process.env.EMAIL_PROVIDER,
+      providerOptions: {
+        host: process.env.EMAIL_SMTP_HOST,
+        port: process.env.EMAIL_SMTP_PORT,
+        auth: {
+          user: process.env.EMAIL_SMTP_USER,
+          pass: process.env.EMAIL_SMTP_PASS,
+        },
+      },
+      settings: {
+        defaultFrom: process.env.EMAIL_ADDRESS_FROM,
+        defaultReplyTo: process.env.EMAIL_ADDRESS_REPLY,
+      },
+    },
   };
 };
